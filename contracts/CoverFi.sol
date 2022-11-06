2e86a3928187f370319929bac5d8cdd570733a23
André Teves <teves@webeleven.com.br>
2022-11-05 23:23:47 -0700

    wip

--- contracts/CoverFi.sol
+++ contracts/CoverFi.sol
@@ -272,43 +272,43 @@ contract CoverFi is Testable, Ownable {
 
             emit ClaimAccepted(claimId, policyId);
             IPUSHCommInterface(0xb3971BCef2D791bc4027BbfedFb47319A4AAaaAa).sendNotification(
-            0x9a09e6a30beaD5f7BF30D59e8468F0009Dd75400, 
-            insurancePolicies[policyId].insuredAddress, 
-            bytes(
-                string(
-                    abi.encodePacked(
-                        "0", 
-                        "+", 
-                        "3", 
-                        "+", 
-                        "Insurance Settled!", 
-                        "+", 
-                        "You are ellible to redeem the insurance and the full amount has been deposited to your account." // notification body
+                0x9a09e6a30beaD5f7BF30D59e8468F0009Dd75400, 
+                insurancePolicies[policyId].insuredAddress, 
+                bytes(
+                    string(
+                        abi.encodePacked(
+                            "0", 
+                            "+", 
+                            "3", 
+                            "+", 
+                            "Insurance Settled!", 
+                            "+", 
+                            "You are ellible to redeem the insurance and the full amount has been deposited to your account." // notification body
+                        )
                     )
                 )
-            )
-        );
+            );
             // Otherwise just reset the flag so that repeated claims can be made.
         } else {
             insurancePolicies[policyId].claimInitiated = false;
-   IPUSHCommInterface(0xb3971BCef2D791bc4027BbfedFb47319A4AAaaAa).sendNotification(
-    0x9a09e6a30beaD5f7BF30D59e8468F0009Dd75400, 
-    insurancePolicies[policyId].insuredAddress, 
-    bytes(
-        string(
-            // We are passing identity here: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
-            abi.encodePacked(
-                "0", // this is notification identity: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
-                "+", // segregator
-                "3", // this is payload type: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/payload (1, 3 or 4) = (Broadcast, targetted or subset)
-                "+", // segregator
-                "Insurance Denied!", // this is notificaiton title
-                "+", // segregator
-                "You are not ellible to redeem the insurance the hack has not been verified." // notification body
-            )
-        )
-    )
-);
+            IPUSHCommInterface(0xb3971BCef2D791bc4027BbfedFb47319A4AAaaAa).sendNotification(
+                0x9a09e6a30beaD5f7BF30D59e8468F0009Dd75400, 
+                insurancePolicies[policyId].insuredAddress, 
+                bytes(
+                    string(
+                        // We are passing identity here: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
+                        abi.encodePacked(
+                            "0", // this is notification identity: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/identity/payload-identity-implementations
+                            "+", // segregator
+                            "3", // this is payload type: https://docs.epns.io/developers/developer-guides/sending-notifications/advanced/notification-payload-types/payload (1, 3 or 4) = (Broadcast, targetted or subset)
+                            "+", // segregator
+                            "Insurance Denied!", // this is notificaiton title
+                            "+", // segregator
+                            "You are not ellible to redeem the insurance the hack has not been verified." // notification body
+                        )
+                    )
+                )
+            );
             emit ClaimRejected(claimId, policyId);
         }
     }
